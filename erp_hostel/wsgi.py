155b0f168e4cd24fe2c69740a6acb1dfb3f30fd2
intellisenseCodez <oyekanmiakande@gmail.com>
2021-11-25 17:34:58 +0100

    Update settings, urls, wsgi files

--- erp_hostel/wsgi.py
+++ erp_hostel/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp_hostel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp_hostel.erp_hostel.settings')
+
 
 application = get_wsgi_application()
